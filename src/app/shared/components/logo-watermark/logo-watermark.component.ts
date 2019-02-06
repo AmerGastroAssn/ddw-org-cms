04e28c4610428166563bdd04da75c1fce5f36d68
cbstodd <cbstodd@gmail.com>
2019-02-06 14:47:49 -0500

    Generated Models and Services for shared and core

---
+++ src/app/shared/components/logo-watermark/logo-watermark.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-watermark',
+  templateUrl: './logo-watermark.component.html',
+  styleUrls: ['./logo-watermark.component.css']
+})
+export class LogoWatermarkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,20 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { LogoWatermarkComponent } from './components/logo-watermark/logo-watermark.component';
 import { MobileFooternavComponent } from './components/mobile-footernav/mobile-footernav.component';
-
-import { SharedRoutingModule } from './shared-routing.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
+
+import { SharedRoutingModule } from './shared-routing.module';
 
 @NgModule({
     declarations: [
         MobileFooternavComponent,
         NavbarComponent,
         SidenavComponent,
+        LogoWatermarkComponent,
     ],
     imports: [
         CommonModule,
         SharedRoutingModule
+    ],
+    exports: [
+        MobileFooternavComponent,
+        NavbarComponent,
+        SidenavComponent,
+        LogoWatermarkComponent,
     ]
 })
 export class SharedModule {}
