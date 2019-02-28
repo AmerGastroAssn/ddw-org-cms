5c3696b9f83288e6bd69eb707c935e8cdf3615a9
cbstodd <cbstodd@gmail.com>
2019-02-28 12:04:23 -0500

    Updated Dashboard to load url in

--- src/app/core/components/analytics-dashboard/analytics-dashboard.component.ts
+++ src/app/core/components/analytics-dashboard/analytics-dashboard.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-analytics-dashboard',
-  templateUrl: './analytics-dashboard.component.html',
-  styleUrls: ['./analytics-dashboard.component.css']
+    selector: 'app-analytics-dashboard',
+    templateUrl: './analytics-dashboard.component.html',
+    styleUrls: ['./analytics-dashboard.component.css']
 })
 export class AnalyticsDashboardComponent implements OnInit {
+    googleUrl: any;
+    url: string;
 
-  constructor() { }
+    constructor(
+        private sanitizer: DomSanitizer,
+    ) {
+        this.url = `https://datastudio.google.com/embed/reporting/15sWOduMA0tqY-oMkfpGtHjd35r4A5GuY/page/1M`;
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.googleUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+    }
 
 }
